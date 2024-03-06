71d881453c7b8d106d1c84d57217491c6f274235
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-03-06 22:49:01 +0500

    init: react, graphQL, shadcnUI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className='text-center text-4xl text-rose-600 bg-rose-200 rounded-xl px-4 py-3'>
+        Bebra
+      </div>
+    </>
+  )
+}
+
+export default App
